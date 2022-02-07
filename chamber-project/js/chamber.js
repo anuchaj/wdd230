c4e0f0d348c1bfc989fbe2d38805fd0bd008617b
anuchaj <anuchaj50@gmail.com>
2022-02-07 19:31:25 +0100

    Chamber Homepage continue

--- chamber-project/js/chamber.js
+++ chamber-project/js/chamber.js
@@ -2,10 +2,16 @@ const d = new Date();
 let currentyear = d.getFullYear();
 let lastdatemodified = document.lastModified;
 
-const day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][new Date().getDay()]
+const day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][new Date().getDay()];
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
+
+let notification = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+
+if (day == "Monday" || day == "Tuesday") {
+  document.getElementById("banner").textContent = notification;
+}
 
 document.getElementById("©").textContent = currentyear;
 document.getElementById("wk-day").textContent = day;
