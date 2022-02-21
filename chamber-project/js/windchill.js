20f0d45eace036561d9d333c6f49bd8b86887cf9
anuchaj <anuchaj50@gmail.com>
2022-02-21 18:27:42 +0100

    WK6 Responsive Design

---
+++ chamber-project/js/windchill.js
@@ -0,0 +1,21 @@
+// Filename windchill.js
+function calculate_wind(temp, speed) {
+    let wind_chill_calculate = 35.74 + (0.6215 * temp) - 35.75 * (speed ** 0.16) + 0.4275 * temp * (speed ** 0.16);
+    return wind_chill_calculate;
+}
+
+
+temp = 30;
+wind_speed = 5;
+document.querySelector(".weather-degree").textContent = temp + " °C";
+
+if (temp <= 50 && wind_speed > 3) {
+  wind_chill = calculate_wind(temp, wind_speed);
+  document.querySelector("#wind-chill").textContent = wind_chill.toFixed(2) + " mph";
+  
+} else {
+    document.querySelector("#wind-chill").textContent = "N/A";
+}
+
+/* wind_chill = calculate_wind(temp, wind_speed);
+console.log(wind_chill); */
