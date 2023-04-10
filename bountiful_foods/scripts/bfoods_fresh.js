7f4af6ce81adc944b75605313f6614626271a335
anuchajz@byui.edu <anuchaj50@gmail.com>
2023-04-10 16:31:05 +0100

    FreshPage update

--- bountiful_foods/scripts/bfoods_fresh.js
+++ bountiful_foods/scripts/bfoods_fresh.js
@@ -1,5 +1,5 @@
-const requestURL = 'https://anuchaj.github.io/wdd230/bountiful_foods/json/fruit.json';
-//const requestURL = "../data/companies.json";
+const requestURL = 'https://anuchaj.github.io/wdd230/bountiful_foods/json/fruits.json';
+//const requestURL = "../json/fruits.json";
 
 fetch(requestURL)
   .then(function (response) {
@@ -16,13 +16,27 @@ fetch(requestURL)
         randomFruits.push(fruit);
         count++;
     }
-    console.log(randomFruits);
+    //console.log(randomFruits);
     randomFruits.forEach(displayCard);
-    //business.forEach(displayCard); 
+    //console.log(carbohydrates);
+    //fruits.forEach(displayCard);
+    
+    //Display the selected/available fruits
+    document.querySelector(".fruit_1").innerHTML = randomFruits[0].name;
+    document.querySelector(".fruit_2").innerHTML = randomFruits[1].name;
+    document.querySelector(".fruit_3").innerHTML = randomFruits[2].name;
+
+    // Display the nutritional facts
+    document.getElementById("carbohydrate").innerHTML = carbohydrates.toFixed(2);
+    document.getElementById("protein").innerHTML = protein.toFixed(2);
+    document.getElementById("fat").innerHTML = fat.toFixed(2);
+    document.getElementById("sugar").innerHTML = sugar.toFixed(2);
+    document.getElementById("calories").innerHTML = calories.toFixed(2);
+
   });
 
 
-let carbohydrate = 0;
+let carbohydrates = 0;
 let protein = 0;
 let fat = 0;
 let sugar = 0;
@@ -35,22 +49,51 @@ function displayCard(fruit) {
     label.setAttribute("class", "fruit_choice");
     let input = document.createElement("input");
     input.setAttribute("type", "checkbox");
+    let br = document.createElement("br");
     
 
-    // Change the textContent property of the h2 element to contain the biz's full name
+    // Change the textContent property of the label element to contain the fruits's info
     label.textContent = fruit.name;
-    carbohydrate += fruit.carbohydrates;
-    protein += fruit.protein;
-    fat += fruit.fat;
-    calories += fruit.calories;
-    sugar += fruit.sugar;
+    carbohydrates += fruit.nutritions.carbohydrates;
+    protein += fruit.nutritions.protein;
+    fat += fruit.nutritions.fat;
+    calories += fruit.nutritions.calories;
+    sugar += fruit.nutritions.sugar;
 
 
-    // Add/append the section(card) with the businessName element
+    // Add/append the label with the input and br elements
     label.appendChild(input);
+    label.appendChild(br);
 
 
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector(".fruits_choice").appendChild(label);
+}
+
+// Adding eventListener to the Make Drink button
+document.getElementById("info_and_nuts").addEventListener("click", cust_info);
+let currentDate = new Date().toJSON().slice(0, 10);
+//console.log(currentDate);
+
+//Track the number of orders or drinks made
+let count = 0;
+
+//Display user's information
+function cust_info() {
+    //Input
+    let fname = document.getElementById("fname").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let text_area = document.getElementById("text_area").value;
+   
+    //Ouput
+    document.querySelector(".cust_name").innerHTML = fname;
+    document.querySelector(".cust_email").innerHTML = email;
+    document.querySelector(".cust_phone").innerHTML = phone;
+    document.querySelector(".special_instruction").innerHTML = text_area;
+    document.querySelector("#order_date").innerHTML = currentDate;
+    document.getElementById('info_and_nuts').style.display='block';
+
+    count++
 }
 
